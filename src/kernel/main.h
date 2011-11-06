d53cd4e3b5201e4b99238704e3759a1cb2163239
David Weinstein <dweinst@me.com>
2011-11-06 18:37:59 -0500

    intial changes to build scripts for osx. not working yet though.

--- src/kernel/main.h
+++ src/kernel/main.h
@@ -4,10 +4,10 @@
 #include "../version.h"
 #include "../build.h"
 #include "kernel.h"
-#include "console\console.h"
-#include "vmx\vmx.h"
-#include "paging\paging.h"
-#include "A20\A20.h"
-#include "interrupt\interrupt.h"
+#include "console/console.h"
+#include "vmx/vmx.h"
+#include "paging/paging.h"
+#include "A20/A20.h"
+#include "interrupt/interrupt.h"
 
 #endif
